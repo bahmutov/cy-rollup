ecc8428b23d29028601b52966d084fedd0259a94
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-06 23:21:55 -0400

    working

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
+import { add } from './calc'
+
 describe('Rollup preprocessor', () => {
   it('works', () => {
-    expect(1).to.equal(1)
+    expect(2).to.equal(2)
+    expect(add(3, 4)).to.equal(7)
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,10 +3,17 @@ const rollup = require('rollup')
 const loadConfigFile = require('rollup/dist/loadConfigFile')
 const path = require('path')
 
+function deepClone(x) {
+  return JSON.parse(JSON.stringify(x))
+}
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = async (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
   const rollupFilename = path.resolve(process.cwd(), 'rollup.config.js')
   const {options, warnings} = await loadConfigFile(rollupFilename)
 
@@ -16,26 +23,66 @@ module.exports = async (on, config) => {
     warnings.flush();
   }
 
-  const rollupOptions = options[0]
-
   // console.log('rollup options')
   // console.log(options)
   // console.log('output options', options[0].output)
 
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  // bundled[filename] => promise
+  const bundled = {}
+
   on('file:preprocessor', async (file) => {
     console.log('preprocessor file %o', file)
 
+    if (bundled[file.filePath]) {
+      console.log('already have bundle promise for file %s', file.filePath)
+      return bundled[file.filePath]
+    }
+
+    const rollupOptions = deepClone(options[0])
     rollupOptions.input = file.filePath
-    const bundle = await rollup.rollup(rollupOptions);
 
     const outputOptions = {
       format: 'iife',
       sourcemap: 'inline',
       file: file.outputPath
     }
-    await bundle.write(outputOptions)
+
+    if (file.shouldWatch) {
+      const watchOptions = {
+        ...rollupOptions,
+        output: outputOptions
+      }
+      const watcher = rollup.watch(watchOptions)
+
+      file.on('close', () => {
+        console.log('file %s close', file.filePath)
+        watcher.close()
+        delete bundled[file.filePath]
+      })
+
+      bundled[file.filePath] = new Promise((resolve, reject) => {
+        watcher.on('event', (e) => {
+          console.log('rollup watcher %s for file %s', e.code, file.filePath)
+          if (e.code === 'END') {
+            resolve(file.outputPath)
+            file.emit('rerun')
+            return
+          }
+
+          if (e.code === 'ERROR') {
+            console.error(e)
+            reject(e)
+          }
+        })
+      })
+
+      await bundled[file.filePath]
+      return file.outputPath
+    }
+
+    const bundle = await rollup.rollup(rollupOptions);
+    bundled[file.filePath] = bundle.write(outputOptions)
+    await bundled[file.filePath]
     return file.outputPath
   })
 }
