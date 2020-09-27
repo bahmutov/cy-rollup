f79cb8aefcc96d896b6d27f666497598bbb96d76
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-27 13:03:43 -0400

    add istanbul plugin

--- examples/code-coverage/rollup.config.js
+++ examples/code-coverage/rollup.config.js
@@ -1,5 +1,13 @@
 // https://rollupjs.org/guide/en/#quick-start
 // https://rollupjs.org/guide/en/#using-config-files
+import istanbul from 'rollup-plugin-istanbul'
+
 export default {
   input: 'src/main.js',
+  plugins: [
+    istanbul({
+      // only instrument app's source files
+      include: ['src/**'],
+    }),
+  ],
 }
